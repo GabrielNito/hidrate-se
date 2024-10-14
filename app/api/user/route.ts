c89f12c1d21f3249f21a9e40628993919a9a4351
Gabriel Nito <nito.gabriel22@gmail.com>
2024-10-13 21:24:57 -0300

    fix: general fixes

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../lib/prisma";
-import { getServerSession } from "next-auth/";
-import { authOptions } from "../../lib/auth";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/lib/auth";
+import { prisma } from "@/app/lib/prisma";
 
 export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
@@ -11,19 +11,37 @@ export async function GET(req: NextRequest) {
   }
 
   const userId = session.user?.id;
-  const user = await prisma.user.findUnique({
-    where: { id: userId },
-    select: {
-      id: true,
-      name: true,
-      email: true,
-      nickname: true,
-    },
-  });
 
-  if (!user) {
-    return NextResponse.json({ message: "User not found" }, { status: 404 });
+  if (!userId) {
+    return NextResponse.json(
+      { message: "User ID not found in session" },
+      { status: 400 }
+    );
   }
 
-  return NextResponse.json(user, { status: 200 });
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        nickname: true,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
 }
