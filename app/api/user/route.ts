c867bb54e02d9993cbfc6ddf79defeb8a236afea
Gabriel Nito <nito.gabriel22@gmail.com>
2024-10-14 01:00:29 -0300

    fix: general fixes

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/lib/auth";
-import { prisma } from "@/app/lib/prisma";
+import prisma from "@/app/lib/prisma";
 
 export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
